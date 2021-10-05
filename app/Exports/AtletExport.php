833e0de16bca00d40d67d5da4b69e2809d759ca4
Ricko Caesar Aprilla Tiaka <bussiness.needs.rctka@gmail.com>
2021-10-05 22:31:18 +0800

    Fixing

--- app/Exports/AtletExport.php
+++ app/Exports/AtletExport.php
@@ -10,27 +10,14 @@ class AtletExport implements FromView
 {
     public function view(): View
     {
+        if(request('search') != 'Semua') {
+            return view('atlet/output/export', [
+                'atlet' => Atlet::where('npci_kota_kabupaten', 'like', '%'. request('search') .'%')->get()
+            ]);
+        }
+
         return view('atlet/output/export', [
             'atlet' => Atlet::all()
         ]);
     }
 }
-
-//class AtletExport implements FromView
-//{
-//    public function view(): View
-//    {
-////        dd(Atlet::all());
-//        return view('atlet/output/export', [
-//            'atlet' => Atlet::all()
-//        ]);
-//    }
-//    public function collection()
-//    {
-////        dd(request('search'));
-//        if(request('search') != 'Semua') {
-//            return Atlet::where('npci_kota_kabupaten', 'like', '%'. request('search') .'%')->get();
-//        }
-//        return Atlet::get();
-//    }
-//}
